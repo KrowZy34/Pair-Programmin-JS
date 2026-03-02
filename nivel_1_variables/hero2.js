c2c516b5208b97a896330d4f2049da0857195b83
ricardorojas <rockeroandre2026@gmail.com>
2026-03-02 10:27:08 -0500

    hero2

---
+++ nivel_1_variables/hero2.js
@@ -0,0 +1,9 @@
+let nombreHeroe = "Ghost Rider";
+let poder = "Fuego infernal";
+let nivelEnergia = "200";
+let ciudad = "Los Angeles"
+let enemigo = "Mefistofeles";
+
+console.log(`${nombreHeroe} es un heroe, su poder es ${poder}.`);
+console.log(`Está defendiendo ${ciudad} de ${enemigo}.`);
+console.log(`Su nivel de energía restante es: ${nivelEnergia}.`);
