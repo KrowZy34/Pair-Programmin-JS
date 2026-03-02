f2b218b6e8f23262401c0c8a9b6f1036c8e7b0aa
KrowZy34 <leobastian562@gmail.com>
2026-03-02 10:13:33 -0500

    chore: heroe1
    se agregó al heroe 1

---
+++ nivel_1_variables/heroe1.js
@@ -0,0 +1,9 @@
+let nombreHeroe = "Batman";
+let poder = "Mucho dinero";
+let nivelEnergia = "90";
+let ciudad = "Gotham";
+let enemigo = "Vein";
+
+console.log(`${nombreHeroe} es un heroe, su poder es ${poder}.`);
+console.log(`Está defendiendo ${ciudad} de ${enemigo}.`);
+console.log(`Su nivel de energía restante es: ${nivelEnergia}.`);
