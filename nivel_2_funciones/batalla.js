4a34ded2fe634fd87fc7ad5a5bedcd3be74aefc9
ricardorojas <rockeroandre2026@gmail.com>
2026-03-02 11:02:00 -0500

    funcion2

--- nivel_2_funciones/batalla.js
+++ nivel_2_funciones/batalla.js
@@ -20,3 +20,16 @@ function superAtaque() {
     console.log(`${heroe2}  lanza un super ataque a ${heroe1} con un daño de ${superA}`);
     return superA;
 }
+
+function recargaEnergia() {
+    recargenergia = vidaHeroe1 + 30;
+    console.log(`${heroe1} recarga su energia , su vida ahora es ${recargenergia} `);
+    return recargenergia
+}
+
+function escudoMagico() {
+    escudomagico = 40;
+    vidaHeroe1 = vidaHeroe1 + escudomagico;
+    console.log(`${heroe1} activa su escudo magico , su vida ahora es ${escudomagico} `);
+    return escudomagico
+}
